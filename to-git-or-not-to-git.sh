93c0a0cba87418495f9c9f3ff552200a243df44f
projetfulle <projetfulle@gmail.com>
2019-06-13 14:48:39 +0000

    Add files via upload

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl https://api.github.com/users | jq ' .[] | select( .login == "projetfulle" )'
+curl  https://api.github.com/users/projetfulle  | grep '"id"' | cut -d : -f 2 | cut -d, -f1 | cut -d " " -f2
